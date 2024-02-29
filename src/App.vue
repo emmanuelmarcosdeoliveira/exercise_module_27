9193e45865cd6e67ebbe6882064119b16cf66adc
Emmmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-02-28 21:17:33 -0300

    :heavy_check_mark: approval test_1

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive } from 'vue';
 import Cabecalho from "./components/Cabecalho.vue"
+import Formulario from './components/Formulario.vue'
 
 const estado = reactive({
   primeiroNumero: "",
@@ -21,69 +22,19 @@ const resultado = () => {
       return parseFloat(estado.primeiroNumero) + parseFloat(estado.segundoNumero);
   }
 }  
-{{estado.primeiroNumero.value}}
 </script>
 
 <template>
   <div class="container">
+    <!-- Cabecalho -->
     <Cabecalho />
-    <!-- Formulário -->
-    <form class="border  rounded-2 p-4 bg-info-subtle">
-      <div class="row  d-flex justify-content-center">
-        <div class="col-md-2">
-         
-          <label for="" class="">Insira um Número</label>
-          <input @keyup="evento => estado.primeiroNumero = evento.target.value"
-          class="form-control" type="number" name="" id="" placeholder="Insira um Numero">
-        </div>
-
-        <div class="col-md-1">
-          <select @change="evento => estado.tipoDeOperacao = evento.target.value" class="form-control mt-4">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-          </select>
-        </div>
-       
-        <!-- Input primeiro Numero -->
-        <div class="col-md-2">
-         <label class="for-control" for="">Insira um Número</label>
-          <input @keyup="evento => estado.segundoNumero = evento.target.value" class="form-control" type="number" name="" id="" placeholder="Inira outro Numero">
-        </div>
-
-        <!-- Input "=" -->
-        <div class="col-md-1">
-         
-          <input class="form-control mt-4" type="text" name="" id="" placeholder="=">
-        </div>
-
-        <!-- Input Segundo Numero -->
-        <div class="col-md-2 meuResultado ">
-       
-       <span  v-if="estado.primeiroNumero && estado.segundoNumero !='' ">{{ resultado().toFixed(2) }} </span>
-       <span v-else>Digite os valores para ver o Resultado</span>
-        </div>
-      </div>
-    </form>
-
-
+        <!-- Formulário -->
+    <Formulario :num1="evento => estado.primeiroNumero = evento.target.value"  :num2="evento => estado.segundoNumero = evento.target.value"  :tipo-deoperacao="evento => estado.tipoDeOperacao = evento.target.value"  :resultado="resultado()" />
+     
   </div>
 </template>
 
 <style scoped>
-::placeholder, option {
-  text-align: center;
-  font-size: 12px;
-}
-.meuResultado {
-  margin-top: 22px;
-  font-size: 24px;
-}
-span {
-  display: block;
-  font-size: 0.75rem;
-  text-wrap: nowrap;
-  padding-top: 10px;
-}
+
+
 </style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,62 @@
+<script setup>
+const props = defineProps(['num1', 'num2', 'tipoDeOperacao', 'resultado'  ])
+</script>
+
+<template>
+
+
+ <form class="border  rounded-2 p-4 bg-info-subtle">
+      <div class="row  d-flex justify-content-center">
+        <div class="col-md-2">
+         
+          <label for="" class="">Insira um Número</label>
+          <input @keyup="props.num1"
+          class="form-control" type="number" name="" id="" placeholder="Insira um Numero">
+        </div>
+
+        <div class="col-md-1">
+          <select @change="props.tipoDeOperacao" class="form-control mt-4">
+            <option value="+">+</option>
+            <option value="-">-</option>
+            <option value="*">*</option>
+            <option value="/">/</option>
+          </select>
+        </div>
+       
+        <!-- Input primeiro Numero -->
+        <div class="col-md-2">
+         <label class="for-control" for="">Insira um Número</label>
+          <input @keyup="props.num2" class="form-control" type="number" name="" id="" placeholder="Inira outro Numero">
+        </div>
+
+        <!-- Input "=" -->
+        <div class="col-md-1">
+         
+          <input class="form-control mt-4" type="text" name="" id="" placeholder="=">
+        </div>
+
+        <!-- Input Segundo Numero -->
+        <div class="col-md-2 meuResultado ">
+       
+          <span v-if="props.num1 || pros.num2 === ''">Digite os valores para ver o Resultado</span>
+       <span  v-else-if="props.num1 && props.num2 !== '' ">{{props.resultado}} </span>
+        </div>
+      </div>
+    </form>
+</template>
+<style scoped>
+::placeholder, option {
+  text-align: center;
+  font-size: 12px;
+}
+.meuResultado {
+  margin-top: 22px;
+  font-size: 24px;
+}
+span {
+  display: block;
+  font-size: 0.75rem;
+  text-wrap: nowrap;
+  padding-top: 10px;
+}
+</style>
